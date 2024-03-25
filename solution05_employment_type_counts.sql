6ca8fe2999640934992d05a7b20006a2398ceeb6
Rowby Pierre <rowbypierre1998@gmail.com>
2024-03-25 04:28:46 -0400

    move files to parent folder

--- solution05_employment_type_counts.sql
+++
@@ -1,9 +0,0 @@
-/*
-5. Count the number of `full-time` positions versus `part-time` or `contract` positions.
-*/
-
-select      count(*)            '# of Positions'
-            , employment_type   'Employment Type'
-from        data_jobs_2024  
-group by    employment_type
-order by    count(*) desc 
--- solution07_workplace_avg_pay.sql
+++
@@ -1,9 +0,0 @@
-/*
-7. How does the average `salary_in_usd` compare between `work_setting` types?
-*/
-
-select      work_setting                                                'Work Setting'
-            ,concat('$', try_cast(avg(salary_in_usd) as numeric(8,2)))  'Average Salary'
-from        data_jobs_2024  
-group by    work_setting
-order by    avg(salary_in_usd) desc ;
